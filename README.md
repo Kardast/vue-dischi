10761d077a2b52b1ffc91ed6d23176f42c0d935c
Kardast <dottorsf@gmail.com>
2022-06-10 15:39:43 +0200

    First commit

--- README.md
+++ README.md
@@ -22,3 +22,18 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+Create un nuovo progetto utilizzando Vue CLI
+
+e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+
+Al caricamento della pagina, effettuate una chiama ajax all’API di boolean: https://flynn.boolean.careers/exercises/api/array/music
+
+e con i dati restituiti, stampate una card per ogni disco musicale.
+
+NOTE:
+mi raccomando provate ad usare le props :occhiolino:
+
+Bonus:
+Creare un componente loader da visualizzare fintantoché i risultati non sono pronti.
---
+++ src/components/ContentContainer.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="content-container">
+    <div class="navbar">
+
+      <img src="../assets/img/logo-small.svg" alt="logo">
+    </div>
+
+    <div class="cards-container">
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContentContainer',
+  // props: {
+
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+  .navbar{
+    background-color: #2e3a46;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    padding-left: 20px;
+
+    img{
+      height: 60px;
+    }
+  }
+
+  .cards-container{
+    background-color: #1e2d3b;
+    height: calc(100vh - 100px);
+  }
+</style>
