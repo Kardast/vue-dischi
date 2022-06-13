e7b99b7cfa8b3fc5c5093a22009b57f071e8927c
Kardast <dottorsf@gmail.com>
2022-06-13 17:06:56 +0200

    Searching by genre

--- src/components/ContentContainer.vue
+++ src/components/ContentContainer.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="content-container">
 
-    <div id="loader" v-if="loading">LOADING ...</div>
+    <SearchBar @mySearch="searchMusic" />
+
+    <div class="loader" v-if="loading">LOADING ...</div>
 
     <div class="cards-container" v-else>
 
-      <MusicCard v-for="(item, index) in musicList" :key="index" :musicObject="item" />
+      <MusicCard 
+        v-for="(item, index) in filteredMusicCards" 
+        :key="index" 
+        :musicObject="item" 
+      />
     </div>
 
   </div>
@@ -14,17 +20,20 @@
 <script>
 import axios from "axios";
 import MusicCard from './MusicCard.vue';
+import SearchBar from "./SearchBar.vue";
 
 export default {
   name: "ContentContainer",
   components: {
-    MusicCard
+    MusicCard,
+    SearchBar
   },
   data() {
     return {
       apiUrl: "https://flynn.boolean.careers/exercises/api/array/music",
       musicList: [],
-      loading: true
+      loading: true,
+      userText: ""
     }
   },
   created() {
@@ -33,15 +42,31 @@ export default {
   methods: {
     getMusicList() {
       axios
-        .get(this.apiUrl)
-        .then((result) => {
-          this.musicList = result.data.response;
-          console.log(this.musicList);
-          this.loading = false;
-        })
-        .catch((error) => {
-          console.log("Errore", error);
-        })
+      .get(this.apiUrl)
+      .then((result) => {
+        this.musicList = result.data.response;
+        console.log(this.musicList);
+        this.loading = false;
+      })
+      .catch((error) => {
+        console.log("Errore", error);
+      })
+    },
+    searchMusic(textUser){
+      this.userText = textUser;
+    }
+  },
+  computed: {
+    filteredMusicCards(){
+      if (this.userText === "") {
+        
+        return this.musicList;
+      } else {
+
+        return this.musicList.filter(item => {
+          return item.genre.toLowerCase().includes(this.userText.toLowerCase());
+        });
+      }
     }
   }
 
@@ -53,16 +78,18 @@ export default {
 .content-container {
   background-color: #1e2d3b;
   height: calc(100vh - 100px);
-  display: flex;
-  justify-content: center;
+  width: 100%;
 
-  #loader{
+  .loader {
     color: white;
-    font-size: 50px;
+    font-size: 40px;
     background-color: #2e475e;
     height: 70px;
     padding: 10px;
-    margin-top: 15px
+    width: 300px;
+    display: flex;
+    margin: 15px auto;
+    justify-content: center;
   }
 
   .cards-container {
@@ -71,6 +98,7 @@ export default {
     flex-wrap: wrap;
     max-width: 1200px;
     padding-top: 30px;
+    margin: 0 auto;
   }
 }
 </style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="search-bar">
+
+    <input type="text" placeholder="Search"
+    
+    v-model="inputText"
+    @keyup.enter="myFirstEmit()"
+    >
+        
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  data (){
+    return {
+        inputText: "",
+    }
+  },
+
+  methods: {
+    myFirstEmit(){
+        this.$emit('mySearch', this.inputText);
+        console.log(this.inputText);
+    }
+    
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.search-bar{
+display: flex;
+justify-content: center;
+padding-top: 10px;
+}
+</style>
